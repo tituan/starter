ad9b14ac3908ed7aea38ac2e11dcb8c5258e6e5e
tituan <antoine.boisard8@gmail.com>
2018-08-11 13:20:44 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang='fr'>
+	<head>
+		<meta charset="UTF-8">
+		<!--[if IE]>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<![endif]-->
+		<meta name="viewport" content="width=device-width initial-scale=1.0">
+		<title>titre</title>
+		<meta name="description" content="Description"/>
+		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+		<link href="dist/css/main.min.css" rel="stylesheet">
+	</head>
+	<body >
+		<header>
+		</header>
+		<main>
+			<h1>Hello</h1>
+		</main>
+		<footer>
+			<script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/additional-methods.min.js"></script>
+			<script src="dist/script/main.min.js"></script>
+		</footer>
+	</body>
+</html>
---
+++ src/script/script.js
@@ -0,0 +1,36 @@
+(function($) {
+  'use strict';
+  ////////////////on load page////////////////////////////////
+
+  // $( "#" ).click(function() {
+  //
+  //   });
+  //
+  // $( "#" ).click(function() {
+  //
+  //   });
+  //
+  // $(document).ready(function(){
+  //
+  //
+  //   if($(window).width() > 1024) {
+  //
+  //     });
+  //   }
+  //   else {
+  //     $( window ).on( "load", function() {
+  //
+  //     });
+  //   }
+  // });
+  
+  //////////////delegation//////////////////////////////////
+
+  // $('body').on("click","#playDesk",function(){
+  //   $('.video').css('display','block');
+  //   $('body').css('overflow','hidden');
+  //   $('.video__container').html('<video src="dist/media/toulouse.mp4" autoplay poster="" class="video__file"></video>');
+  //
+  // });
+
+})(jQuery);
